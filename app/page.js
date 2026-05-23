933149ddcc96cd98ec6915252f96b7f19b0518fb
ladexo <ololadesuberu@gmail.com>
2026-05-22 21:31:29 -0700

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import AnalyzerDashboard from '@/components/AnalyzerDashboard';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-slate-950 flex flex-col items-center justify-center p-4">
+      <AnalyzerDashboard />
+    </main>
+  );
+}
